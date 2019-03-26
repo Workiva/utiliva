9020e53f88c3b2bb619c0fecd31c8565719d67f2
Tyler Wilding <tyler.wilding@workiva.com>
2019-03-26 16:04:41 -0400

    Do not require GPG keys when pushing to Clojars

--- project.clj
+++ project.clj
@@ -1,13 +1,20 @@
 (defproject com.workiva/utiliva "0.2.0"
   :description "Utilia Workivinarum"
   :url "https://github.com/Workiva/utiliva"
+
   :license {:name "Eclipse Public License 1.0"}
+
   :plugins [[lein-shell "0.5.0"]
             [lein-codox "0.10.3"]]
+
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.cache "0.6.5"]
                  [com.workiva/tesserae "1.0.0"]
                  [backtick "0.3.4"]]
+
+  :deploy-repositories {"clojars"
+                        {:url "https://repo.clojars.org"
+                         :sign-releases false}}
 
   :source-paths      ["src"]
   :test-paths        ["test"]
